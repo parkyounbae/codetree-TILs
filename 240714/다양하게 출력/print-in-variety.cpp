dfc6205ce2dc6b67e6377db08a5cb07eb69ea7c5
parkyounbae <55349548+parkyounbae@users.noreply.github.com>
2024-07-14 14:41:21 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240714/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cout << "Total days in Year\n"
+            "365\n"
+            "Circumference rate\n"
+            "3.1415926535\n";
+    return 0;
+}
