14f5375fb545e221627e30c7c0b3ee9ca5f328f2
parkyounbae <55349548+parkyounbae@users.noreply.github.com>
2024-07-18 17:20:47 +0900

    Title: 기울어진 직사각형의 회전, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240718/기울어진 직사각형의 회전/rotate-slanted-rectangle.cpp
+++ 240718/기울어진 직사각형의 회전/rotate-slanted-rectangle.cpp
@@ -41,7 +41,39 @@ void move1(int r, int c, int m1, int m2, int m3, int m4) {
 }
 
 void move2(int r1, int c1, int r2, int c2, int temp) {
-    
+    int index_r = r;
+    int index_c = c;
+
+    int temp2 = arr[index_r-m2][index_c-m2];
+    for(int i=m2 ; i>0 ; i--) {
+        arr[index_r-i][index_c-i] = arr[index_r-i+1][index_c-i+1];
+    }
+
+    index_r = index_r-m2;
+    index_c = index_c-m2;
+
+    int temp1 = arr[index_r-m1][index_c+m1];
+    for(int i=m1 ; i>0 ; i--) {
+        arr[index_r-i][index_c+i] = arr[index_r-i+1][index_c+i-1];
+    }
+    arr[index_r-1][index_c+1] = temp2;
+
+    index_r = index_r-m1;
+    index_c = index_c+m1;
+
+    int temp4 = arr[index_r+m4][index_c+m4];
+    for(int i=m4 ; i>1 ; i--) {
+        arr[index_r+i][index_c+i] = arr[index_r+i-1][index_c+i-1];
+    }
+    arr[index_r+1][index_c+1] = temp1;
+
+    index_r = index_r+m4;
+    index_c = index_c+m4;
+
+    for(int i=m3 ; i>1 ; i--) {
+        arr[index_r+i][index_c-i] = arr[index_r+i-1][index_c-i+1];
+    }
+    arr[index_r+1][index_c-1] = temp4;
 }
 
 int main() {
@@ -58,6 +90,8 @@ int main() {
 
     if(dir == 0) {
         move1(r,c,m1,m2,m3,m4);
+    } else {
+        move2(r,c,m1,m2,m3,m4);
     }
 
     for(int i=1 ; i<=n ; i++) {
