d2931d0a8a981f383107845c558732765f5ce4ee
parkyounbae <55349548+parkyounbae@users.noreply.github.com>
2024-07-21 15:12:49 +0900

    Title: 1차원 폭발 게임, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240721/1차원 폭발 게임/The-1D-bomb-game.cpp
+++ 240721/1차원 폭발 게임/The-1D-bomb-game.cpp
@@ -8,7 +8,12 @@ int arr[101];
 void re_sort(int delete_count) {
     int deleted_count = 0;
     int index = 1;
-    while(deleted_count != delete_count) {
+//    int loop_count = 0;
+    while(deleted_count < delete_count) {
+//        loop_count++;
+//        if(loop_count>10) {
+//            break;
+//        }
         int start = -1;
         int end = -1;
 
@@ -23,11 +28,13 @@ void re_sort(int delete_count) {
             if(arr[i] != -1) {
                 end = i-1;
                 break;
+            } else if(i==result && arr[i]==-1) {
+                end = result;
             }
         }
 
         for(int i=start ; i<=result ; i++) {
-            if(+(end-start+1) <= result) {
+            if(i+(end-start+1) <= result) {
                 arr[i] = arr[i+(end-start+1)];
             } else {
                 arr[i] = 0;
@@ -36,35 +43,26 @@ void re_sort(int delete_count) {
 
         deleted_count += (end-start+1);
         result = result - (end-start+1);
-
-        // cout << "deleted_count : " << deleted_count << endl;
-        // cout << "result : " << result << endl;
-
-        // for(int i=1 ; i<=result ; i++) {
-        //     cout << arr[i] << " ";
-        // }
     }
 }
 
 void bomb() {
-    int delete_count = 0;
-    while(result != 0) {
-        int index = arr[1];
-        int count = 1;
-        for(int i=1 ; i<result ; i++) {
-            if(index == arr[i+1]) {
-                count ++;
-            } else {
-                if(count >= m) {
-                    for(int j=0 ; j<count ; j++) {
-                        arr[i-j] = -1;
-                    }
-                    delete_count += count;
-                } 
-                count = 1;
-                index = arr[i+1];
+    while(true) {
+        int delete_count = 0;
+        int index_value = arr[1];
+        int index_key = 1;
+        for(int i=1 ; i<= result ; i++) {
+            if(index_value == arr[i] && (i-index_key+1)>=m) {
+                for(int j=i ; j>=index_key&&arr[j]!=-1; j--) {
+                    arr[j] = -1;
+                    delete_count++;
+                }
+            } else if(index_value != arr[i]) {
+                index_key = i;
+                index_value = arr[i];
             }
         }
+
         if(delete_count == 0) {
             break;
         } else {
@@ -81,6 +79,11 @@ int main() {
 
     for(int i=1 ; i<=n ; i++) {
         cin >> arr[i];
+    }
+
+    if(m==1) {
+        cout << "0" << endl;
+        return 0;
     }
 
     bomb();
