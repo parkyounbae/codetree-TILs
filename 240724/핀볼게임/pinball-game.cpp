bcae15ef3199c1142b2424195774f74750ad6c69
parkyounbae <55349548+parkyounbae@users.noreply.github.com>
2024-07-24 16:15:41 +0900

    Title: 핀볼게임, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240724/핀볼게임/pinball-game.cpp
+++ 240724/핀볼게임/pinball-game.cpp
@@ -2,59 +2,41 @@
 using namespace std;
 
 int n;
-int arr[101][101];
+int arr[102][102];
 
 int dir_r[4] = {-1, 1, 0, 0};
 int dir_c[4] = {0,0,-1,1};
-int check[100][100];
 
 int result = 0;
 
-void clear_check() {
-    for(int i=0 ; i<n ; i++) {
-        for(int j=0 ; j<n ; j++) {
-            check[i][j] = 0;
-        }
-    }
-}
+
 
 int pin_ball(int start_r, int start_c, int start_dir) {
     int time = 0;
     int current_r = start_r;
     int current_c = start_c;
     int current_dir = start_dir;
-    clear_check();
     while(true) {
-        
+
         time++;
         current_r = current_r + dir_r[current_dir];
         current_c = current_c + dir_c[current_dir];
 
-        if(current_r<0 || current_r>=n || current_c<0 || current_c>=n) {
+        if(current_r<=0 || current_r>n || current_c<=0 || current_c>n) {
             break;
         }
 
         if(arr[current_r][current_c] == 1) {
-            if(check[current_r][current_c] == 2) {
-                return -1;
-            } else {
-                check[current_r][current_c] ++;
-            }
             if(current_dir == 0) {
-                current_dir =3;
+                current_dir = 3;
             } else if(current_dir == 1) {
-                current_dir =2;
+                current_dir = 2;
             } else if(current_dir == 2) {
-                current_dir =1;
+                current_dir = 1;
             } else if(current_dir == 3) {
-                current_dir =0;
+                current_dir = 0;
             }
         } else if(arr[current_r][current_c] == 2) {
-            if(check[current_r][current_c] == 2) {
-                return -1;
-            } else {
-                check[current_r][current_c] ++;
-            }
             if(current_dir == 0) {
                 current_dir =2;
             } else if(current_dir == 1) {
@@ -79,13 +61,7 @@ int main() {
         }
     }
 
-    for(int i=0 ; i<4 ; i++) {
-        for(int j=0 ; j<n ; j++) {
-
-        }
-    }
-
-    for(int i=0 ; i<n ; i++) {
+    for(int i=1 ; i<=n ; i++) {
         int temp = pin_ball(0,i,1);
 
         if(temp > result) {
@@ -98,13 +74,13 @@ int main() {
             result = temp2;
         }
 
-        int temp3 = pin_ball(n, i, 0);
+        int temp3 = pin_ball(n+1, i, 0);
 
         if(temp3 > result) {
             result = temp3;
         }
 
-        int temp4 = pin_ball(i,n,2);
+        int temp4 = pin_ball(i,n+1,2);
 
         if(temp4 > result) {
             result = temp4;
